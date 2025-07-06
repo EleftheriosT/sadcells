f73b76f722f1f625f769fab7f653ae182afa5072
Ben Martin <bmartin12@btinternet.com>
2025-07-06 17:43:18 +0100

    version click mage man

--- Game/enemies/area_2d.gd
+++ Game/enemies/area_2d.gd
@@ -7,38 +7,34 @@ var moral = 5
 
 @export var exclusive = true
 @export var selection_action = "select"
-var selected: bool = false:
-	set = set_selected
 
-#handles the "selected group" to ensure that units are added/removed when clicked off
+var _selected := false  # backing variable
+var selected: bool:
+	set = set_selected,
+	get = get_selected
+
+signal selection_toggled(selection)
+
 func set_selected(selection):
 	if selection:
 		_make_exclusive()
 		add_to_group("selected")
 	else:
-		remove_from_group("selected") 
-	selected = selection
+		remove_from_group("selected")
+	_selected = selection
+	emit_signal("selection_toggled", selection)
 
-# Checks to see if things arent sleected already
+func get_selected():
+	return _selected
+
 func _make_exclusive():
-	if not selected:
+	if not _selected:
 		return
 	get_tree().call_group("selected", "set_selected", false)
 
-#changes selection type, if selected will unselect and visa versa
 func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
 	if event.is_action_pressed("LeftClick"):
-		set_selected(not selected)
+		selected = not selected
 
-
-
-
- 
-var mousePos = Vector2()
-var s = 0.5  
-
-var mousePosGlobal = Vector2()
-
-
-
-signal selection_toggled(selection)
+func _on_selection_toggled(selection: Variant) -> void:
+	$"../Label".visible = selection  # fixed typo
